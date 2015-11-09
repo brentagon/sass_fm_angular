177f1048c1a5e0859a459f39ee64a335a325bb2a
Brent Bailey <baileyb@alumni.reed.edu>
2015-11-09 15:47:13 -0500

    allow user to store JSON data in factory

--- js/app.js
+++ js/app.js
@@ -1,15 +1,72 @@
 'use strict';
 
+angular.module('myApp', ['ui.router'])
+	.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('home', {
+					url: '/home',
+					templateUrl: '/home.html',
+					controller: 'SearchController'
+				});
+				.state('albums', {
+					url: '/albums/{id}',
+					templateUrl: '/albums.html',
+					controller: 'AlbumsController'
+				});
+			$urlRouterProvider.otherwise('home');
+		}]);
 
-angular.module('myApp', [])
-	.controller('SearchController', function($scope, $http){
+	.factory('albums', [function(){
+		var a = {
+			albums: []
+		};
+		return a;
+	}]);
+
+	.controller('AlbumsController', [
+		'$scope',
+		'$stateParams',
+		'albums',
+		function($scope, $stateParams, albums) {
+			$scope.addReview = function() {
+				if($scope.body === '') { return; }
+				$scope.album.reviews.push({
+					body: $scope.body,
+					author: 'user',
+					upvotes: 0
+				});
+				$scope.body = '';
+			}
+		}]);
+
+	.controller('SearchController', [
+	'$scope',
+	'albums',
+	'reviews'
+	function($scope, albums, $http){
 
 	var pendingTask;
+	$scope.albums = albums.albums[$stateParams.id];
+	$scope.reviews = reviews.reviews;
 
 	if($scope.search === undefined){
 		$scope.search = "";
 		fetch();
 	}
+	$scope.addAlbum = function() {
+		$scope.albums.push({
+			title: $scope.details[0].title,
+			img: $scope.details[0].thumb,
+			reviews: [
+				{author: 'Bront', body: 'Whatever', upvotes: 0},
+				{author: 'Brandt', body: 'I love it', upvotes: 0}
+			]
+		});
+		console.log($scope.albums);
+	};
 
 	$scope.change = function(){
 		if(pendingTask){
@@ -23,7 +80,7 @@ angular.module('myApp', [])
 		var output = response.results;
 		$scope.details = output;
 		var related = output.slice(1, (output.length - 1));
-		$scope.results = related; console.log(related); });
+		$scope.results = related; });
 		/* do something for related results, eventually use Grunt to store secret & key */
 	}
 
@@ -35,4 +92,4 @@ angular.module('myApp', [])
 	$scope.select = function() {
 		this.setSelectionRange(0, this.value.length);
 	}
-});
+}]);
